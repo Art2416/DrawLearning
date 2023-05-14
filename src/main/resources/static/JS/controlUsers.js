c4095f833ec2de66351a18288cf442f87e2691c5
stefania5964 <tefila123@hotmail.com>
2023-05-14 18:22:00 -0500

    login sin terminar

--- src/main/resources/static/JS/controlUsers.js
+++ src/main/resources/static/JS/controlUsers.js
@@ -1,10 +1,22 @@
-async function signIn() {
-  const isAdmin = confirm("¿Eres Admin?");
-  const user = isAdmin ? "Admin" : "User";
-  localStorage.setItem("logeo", user);
-  if (user === "Admin") {
-    app.createMaster(user);
-  } else {
-    app.createUser(user);
-  }
+async function signIn(){
+    const config = {
+        auth: {
+            clientId: '498cc182-a2f7-4de1-950c-85367adad63e',
+            authority: 'https://login.microsoftonline.com/common/',
+            redirectUri: 'https://localhost:8080/index.html'
+        }
+    };
+    var client = new Msal.UserAgentApplication(config);
+    var request = {
+        scopes: ['user.read']
+    };
+    let loginResponse = await client.loginPopup(request);
+    console.log("hola bienvenido")
+    localStorage.setItem("logeo", loginResponse.account.name)
+    if (loginResponse.account.name === "Organizer"){
+        app.createOrganizer(loginResponse.account.name)
+    }else{
+
+        app.createUser(loginResponse.account.userName)
+    }
 }
