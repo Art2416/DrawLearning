dbf58dc3fb2d2630023e8e882f9f95e61536ab78
sgoes <david.narvaez@mail.escuelaing.edu.co>
2023-05-14 11:03:50 -0500

    new uniter test

--- src/test/java/edu/eci/arsw/services/DrawLearningServicesTest.java
+++ src/test/java/edu/eci/arsw/services/DrawLearningServicesTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
 
 public class DrawLearningServicesTest {
     InMemoryDrawLearningPersistence dp;
-    User usuario1, usuario2, usuario3, resultado;
+    User david, stefania, ivan, result;
 
     @Before
     public void before(){
@@ -24,180 +24,180 @@ public class DrawLearningServicesTest {
     }
 
     @Test
-    public void deberiaGuardarUsuario() throws DrawLearningPersistenceException {
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(3, 4);
-        Point p3 = new Point(5, 6);
-        puntosPrueba.add(p1);
-        puntosPrueba.add(p2);
-        puntosPrueba.add(p3);
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        resultado = dp.getUser("UsuarioPrueba");
-        assertEquals(usuario1, resultado);
+    public void newMaster(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUserOrganizer", testPoints);
+        dp.saveUser(david);
+        assertEquals(david, dp.getOrganizerName());
     }
 
     @Test
-    public void deberiaGuargarTodosUsuarios(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba1", puntosPrueba);
-        usuario2 = new User("UsuarioPrueba2", puntosPrueba);
-        usuario3 = new User("UsuarioPrueba3", puntosPrueba);
-        dp.saveUser(usuario1);
-        dp.saveUser(usuario2);
-        dp.saveUser(usuario3);
+    public void noSaveMaster(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        assertEquals(null, dp.getOrganizerName());
+    }
+
+    @Test
+    public void saveUser() throws DrawLearningPersistenceException {
+        ArrayList<Point> testPoints  = new ArrayList();
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5, 6);
+        testPoints.add(p1);
+        testPoints.add(p2);
+        testPoints.add(p3);
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        result = dp.getUser("testUser");
+        assertEquals(david, result);
+    }
+
+    @Test
+    public void saveAllUser(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser1", testPoints);
+        stefania = new User("testUser2", testPoints);
+        ivan = new User("testUser3", testPoints);
+        dp.saveUser(david);
+        dp.saveUser(stefania);
+        dp.saveUser(ivan);
         Set<User> usuarios = dp.getAllUsers();
         assertEquals(usuarios.size(), 3);
     }
 
     @Test
-    public void deberiaObtenerPuntosCorrectamente() throws DrawLearningPersistenceException {
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(3, 4);
-        Point p3 = new Point(5, 6);
-        puntosPrueba.add(p1);
-        puntosPrueba.add(p2);
-        puntosPrueba.add(p3);
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        resultado = dp.getUser("UsuarioPrueba");
-        assertEquals(puntosPrueba, resultado.getPoints());
-    }
-
-    @Test
-    public void deberiaObtenerPuntosUsuarioCorrecto(){
-        ArrayList<Point> puntosPrueba1  = new ArrayList();
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(3, 4);
-        puntosPrueba1.add(p1);
-        puntosPrueba1.add(p2);
-        ArrayList<Point> puntosPrueba2  = new ArrayList();
-        Point p3 = new Point(5, 6);
-        Point p4 = new Point(7, 8);
-        puntosPrueba2.add(p3);
-        puntosPrueba2.add(p4);
-        usuario1 = new User("UsuarioPrueba1", puntosPrueba1);
-        usuario2 = new User("UsuarioPrueba2", puntosPrueba2);
-        dp.saveUser(usuario1);
-        dp.saveUser(usuario2);
-        assertEquals(puntosPrueba2, dp.getPointsByUser("UsuarioPrueba2"));
-    }
-
-    @Test
-    public void deberiaAgregarPuntoCorrectamente(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        Point p1 = new Point(1, 2);
-        puntosPrueba.add(p1);
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.addPointToUser(usuario1);
-        assertEquals(puntosPrueba, dp.getPointsByUser("UsuarioPrueba"));
-    }
-
-    @Test
-    public void deberiaBorrarTodosPuntos() throws DrawLearningPersistenceException {
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(3, 4);
-        Point p3 = new Point(5, 6);
-        puntosPrueba.add(p1);
-        puntosPrueba.add(p2);
-        puntosPrueba.add(p3);
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        dp.delteAllPointsUser("UsuarioPrueba");
-        assertEquals(0, dp.getUser("UsuarioPrueba").getPoints().size());
-    }
-
-    @Test
-    public void deberiaRetornarUsuarioGanador(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        dp.setWinner("UsuarioPrueba");
-        assertEquals(usuario1, dp.getWinner());
-    }
-
-    @Test
-    public void deberiaEliminarParticipante(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba1", puntosPrueba);
-        usuario2 = new User("UsuarioPrueba2", puntosPrueba);
-        usuario3 = new User("UsuarioPrueba3", puntosPrueba);
-        dp.saveUser(usuario1);
-        dp.saveUser(usuario2);
-        dp.saveUser(usuario3);
+    public void deleteUser(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser1", testPoints);
+        stefania = new User("testUser2", testPoints);
+        ivan = new User("testUser3", testPoints);
+        dp.saveUser(david);
+        dp.saveUser(stefania);
+        dp.saveUser(ivan);
         dp.deleteParticipantes();
         assertEquals(0, dp.getAllUsers().size());
     }
 
     @Test
-    public void deberiaCrearPista() throws DrawLearningPersistenceException {
-        Clue pist = new Clue("Buen juego", false);
-        dp.saveClue(pist);
-        assertEquals("Buen juego", pist.getContenido());
-    }
-
-    @Test
-    public void deberiaTomarPista() throws DrawLearningPersistenceException {
-        Clue pist = new Clue("Buen juego", false);
-        dp.saveClue(pist);
-        assertEquals("Buen juego", dp.TakeClue());
-    }
-
-    @Test
-    public void noDeberiaSeleccionarOtroGanadorMismaPartida(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba1", puntosPrueba);
-        dp.saveUser(usuario1);
-        dp.setWinner("UsuarioPrueba1");
-        usuario2 = new User("UsuarioPrueba2", puntosPrueba);
-        dp.saveUser(usuario2);
-        dp.setWinner("UsuarioPrueba2");
-        assertNotEquals(usuario2, dp.getWinner());
-    }
-
-    @Test
-    public void nodeberiaGuadarUsuarioDosVeces() throws DrawLearningPersistenceException {
-        ArrayList<Point> puntosPrueba1  = new ArrayList();
+    public void noSaveUserTwice() throws DrawLearningPersistenceException {
+        ArrayList<Point> testPoints1  = new ArrayList();
         Point p1 = new Point(1, 2);
-        ArrayList<Point> puntosPrueba2  = new ArrayList();
+        ArrayList<Point> testPoints2  = new ArrayList();
         Point p2 = new Point(3, 4);
-        puntosPrueba1.add(p1);
-        puntosPrueba2.add(p2);
-        usuario1 = new User("UsuarioPrueba", puntosPrueba1);
-        usuario2 = new User("UsuarioPrueba", puntosPrueba2);
-        dp.saveUser(usuario1);
-        dp.saveUser(usuario2);
-        resultado = dp.getUser("UsuarioPrueba");
-        assertNotEquals(usuario2, resultado);
+        testPoints1.add(p1);
+        testPoints2.add(p2);
+        david = new User("testUser", testPoints1);
+        stefania = new User("testUser", testPoints2);
+        dp.saveUser(david);
+        dp.saveUser(stefania);
+        result = dp.getUser("testUser");
+        assertNotEquals(stefania, result);
     }
 
     @Test
-    public void deberiaCambiarPista() throws DrawLearningPersistenceException {
-        Clue pist1 = new Clue("Buen juego", false);
+    public void getPoints() throws DrawLearningPersistenceException {
+        ArrayList<Point> testPoints  = new ArrayList();
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5, 6);
+        testPoints.add(p1);
+        testPoints.add(p2);
+        testPoints.add(p3);
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        result = dp.getUser("testUser");
+        assertEquals(testPoints, result.getPoints());
+    }
+
+    @Test
+    public void getUserPoints(){
+        ArrayList<Point> testPoints1  = new ArrayList();
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        testPoints1.add(p1);
+        testPoints1.add(p2);
+        ArrayList<Point> testPoints2  = new ArrayList();
+        Point p3 = new Point(5, 6);
+        Point p4 = new Point(7, 8);
+        testPoints2.add(p3);
+        testPoints2.add(p4);
+        david = new User("testUser1", testPoints1);
+        stefania = new User("testUser2", testPoints2);
+        dp.saveUser(david);
+        dp.saveUser(stefania);
+        assertEquals(testPoints2, dp.getPointsByUser("testUser2"));
+    }
+
+    @Test
+    public void savePoint(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        Point p1 = new Point(1, 2);
+        testPoints.add(p1);
+        david = new User("testUser", testPoints);
+        dp.addPointToUser(david);
+        assertEquals(testPoints, dp.getPointsByUser("testUser"));
+    }
+
+    @Test
+    public void deleteAllPoints() throws DrawLearningPersistenceException {
+        ArrayList<Point> testPoints  = new ArrayList();
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5, 6);
+        testPoints.add(p1);
+        testPoints.add(p2);
+        testPoints.add(p3);
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        dp.delteAllPointsUser("testUser");
+        assertEquals(0, dp.getUser("testUser").getPoints().size());
+    }
+
+    @Test
+    public void returnWinner(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser", testPoints);
+        dp.saveUser(david);
+        dp.setWinner("testUser");
+        assertEquals(david, dp.getWinner());
+    }
+
+    @Test
+    public void newClue() throws DrawLearningPersistenceException {
+        Clue pist = new Clue("good game", false);
+        dp.saveClue(pist);
+        assertEquals("good game", pist.getContenido());
+    }
+
+    @Test
+    public void takeClue() throws DrawLearningPersistenceException {
+        Clue pist = new Clue("good game", false);
+        dp.saveClue(pist);
+        assertEquals("good game", dp.TakeClue());
+    }
+
+    @Test
+    public void onlyOneWinner(){
+        ArrayList<Point> testPoints  = new ArrayList();
+        david = new User("testUser1", testPoints);
+        dp.saveUser(david);
+        dp.setWinner("testUser1");
+        stefania = new User("testUser2", testPoints);
+        dp.saveUser(stefania);
+        dp.setWinner("testUser2");
+        assertNotEquals(stefania, dp.getWinner());
+    }
+
+    @Test
+    public void changeClue() throws DrawLearningPersistenceException {
+        Clue pist1 = new Clue("good game", false);
         dp.saveClue(pist1);
-        Clue pist2 = new Clue("Mal juego", false);
+        Clue pist2 = new Clue("bad game", false);
         dp.saveClue(pist2);
-        assertEquals("Mal juego", dp.TakeClue());
-    }
-
-    @Test
-    public void deberiaAgregarMaster(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPruebaOrganizer", puntosPrueba);
-        dp.saveUser(usuario1);
-        assertEquals(usuario1, dp.getOrganizerName());
-    }
-
-    @Test
-    public void noDeberiaSerMaster(){
-        ArrayList<Point> puntosPrueba  = new ArrayList();
-        usuario1 = new User("UsuarioPrueba", puntosPrueba);
-        dp.saveUser(usuario1);
-        assertEquals(null, dp.getOrganizerName());
+        assertEquals("bad game", dp.TakeClue());
     }
 }
